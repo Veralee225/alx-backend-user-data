ff288d697524c15ebeac1ec4208aefb727bf713f
Veralee225 <Veralee225@gmail.com>
2023-02-08 04:42:30 +0100

    feat: error handling

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,13 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route("/authorized/", strict_slashes=False)
+def unauthorized() -> str:
+  """Tests the 401 error handler"""
+  abort(401)
+
+@app_views.route("/forbidden/", strict_slashes=False)
+def forbidden() -> str:
+  """Tests the 403 error handler"""
+  abort(403)
