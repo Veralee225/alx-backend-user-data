1ff7e7c7feee362eb12c806d213b07160da4a640
Veralee225 <Veralee225@gmail.com>
2023-02-11 18:45:25 +0100

    fix: lint fix

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,7 +25,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-@app_views.route("/authorized/", strict_slashes=False)
+@app_views.route("/unauthorized/", strict_slashes=False)
 def unauthorized() -> str:
   """Tests the 401 error handler"""
   abort(401)
