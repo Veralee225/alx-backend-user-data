b68fc18e6b679966378a4460d8f31458b55fec2c
Veralee225 <Veralee225@gmail.com>
2023-02-12 01:25:45 +0100

    fix: lint

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,7 +12,8 @@ class BasicAuth(Auth):
     """BasicAuth class """
 
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        """Get the Base64 part of the Authorization header for a Basic Authentication"""
+        """Get the Base64 part of the Authorization header 
+        for a Basic Authentication"""
         if not authorization_header:
             return None
         if not isinstance(authorization_header, str):
